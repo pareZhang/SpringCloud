99e35d7edb737ee02ecc3a87959eb6158fa203c2
zjm <pareZhang@126.com>
2019-12-12 10:06:37 +0800

    在RestTemplate和Ribbon上使用熔断器

--- eureka-ribbon-client/src/main/java/com/zjm/service/RibbonService.java
+++ eureka-ribbon-client/src/main/java/com/zjm/service/RibbonService.java
@@ -1,6 +1,8 @@
 package com.zjm.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +14,21 @@ import org.springframework.web.client.RestTemplate;
 public class RibbonService {
     @Autowired
     private RestTemplate restTemplate;
+    /**
+     * 使用RestTemplate和Ribbon消费服务
+     * public String hello(String name){
+        return restTemplate.getForObject("http://eureka-client/hello?name="+name,String.class);
+    }*/
+    /**
+     * 在RestTemplate和Ribbon上使用熔断器
+     * @param name
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "helloError")
     public String hello(String name){
         return restTemplate.getForObject("http://eureka-client/hello?name="+name,String.class);
+    }
+    public String helloError(String name){
+        return "hello,"+name+",sorry,error!";
     }
 }
